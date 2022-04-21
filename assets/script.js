96ec5a789aa98cc6e7b00d69cd3c5237a8a78b5a
HunterLM <hunter109@gmail.com>
2022-04-21 15:46:28 -0500

    added comments and tried new script

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,33 @@
+// quiz questions obj 
 var quizQuestions = [
     {
         title: "What is a useful tool in debugging problems in your code?",
         options: ["Browser Dev Tools", "Pro Debugger v3.36", "Review your code", "Quick Debugger App"],
-        answer: "Browser Dev Tools"
+        answer: 0
     },
     {
         title: "Arrays in JavaScript are used to store?",
         options: ["String of content", "More than one value", "Functions", "Cookies"],
-        answer: "More than one value"
+        answer: 1
     },
     {
         title: "What is used to make comments in JavaScript?",
         options: ["/*", "$$", "##", "//"],
-        answer: "//"
+        answer: 3
     },
     {
         title: "A TRUE or FALSE statement is what?",
         options: ["String", "Variable", "Boolean", "Array"],
-        answer: "Boolean"
+        answer: 2
     },
     {
         title: "What does the CONST variable do?",
         options: ["Stores data that can change", "Constructs your JavaScript", "Stores unchangable data", "Starts a function"],
-        answer: "Stores unchangable data"
+        answer: 2
     } 
 ];
 
+// elements from index.html that I'll need 
 var startBtn = document.getElementById("startBtn");
 var timerEl = document.getElementById("timer");
 var game = document.getElementById("game");
@@ -36,79 +38,87 @@ var savedInitialsEl = document.getElementById("savedInitials");
 var savedScoreEl = document.getElementById("savedScore");
 var showScoreEl = document.getElementById("showScore");
 
-
+// make elements 
 makeUl = document.createElement("ul", {is : "ul"});
 makeLi = document.createElement("li", {is : "li"});
 makeH1 = document.createElement("h1", {is : "h1"});
 makeP = document.createElement("p", {is: "p"});
 
+// game variables for score and time
 var timeRemaining = 40;
 var timeDeduction = 10;
 var score = 0;
 var secondsLeft = 40;
 var holdInterval = 0;
 
-// jquery ins 09 might be useful
-
-//startBtn.addEventListener("click", makeQuizQuestions);
-
-
+// on start button click, start timer, call makeStuffHappen
 startBtn.addEventListener("click", function() {
     if (holdInterval === 0) {
         holdInterval = setInterval(function() {
             secondsLeft--;
             timerEl.textContent = "Time Left: " + secondsLeft;
-
+            // when timer runs out gameOver
             if (secondsLeft <= 0) {
                 clearInterval(holdInterval);
                 gameOver();
                 timerEl.textContent = "Out of Time!";
             }
-        }, 1000);
+        }, 1000); // every second update
     }
-    makeStuffHappen();
+    makeStuffHappen(); // call function to populate questions
 });
 
-function makeStuffHappen() {
+// function to populate questions
+function makeStuffHappen(questionIndex, optionsIndex, currentQuestion) {
     game.innerHTML = "";
     gameH1.innerHTML = "";
     gameP.remove();
     startBtn.remove();
-    optionsUl = "";
+    // optionsUl.innerHTML = "";
 
+// trying to store quizQuestion data into new variables ***not working right***
     for (var i = 0; i < quizQuestions.length; i++) {
-        
-        var questionIndex = quizQuestions[i].title;
-        var optionsIndex = quizQuestions[i].options;
-        gameH1.textContent = questionIndex;
+        var currentQuestion = 0;
+        var questionIndex = quizQuestions[currentQuestion].title;
+        var optionsIndex = quizQuestions[currentQuestion].options;
+        gameH1.textContent = quizQuestions[currentQuestion].title;
     };
     console.log(questionIndex);
     console.log(optionsIndex);
-
-    optionsIndex.forEach(function (newLi) {
+// taking options index array, for each item make new <li>
+    optionsIndex.forEach(function (newLi, element, index) {
         var optionsUl = document.getElementById("optionsUl");
         var makeLi = document.createElement("li");
+        var options = document.querySelectorAll("options");
         optionsUl.appendChild(makeLi);
         makeLi.textContent = newLi;
+        // adding event listener to new li and checking if it's correct
         makeLi.addEventListener("click", (isRightNot));
     });
 
-    
 
 };
 
-function isRightNot(event) {
-    // event.preventDefault();
+// check to see if clicked answer is correct 
+function isRightNot(event, questionIndex, optionsIndex, currentQuestion) {
+    event.preventDefault();
 
-    var element = event.target;
+    var element = event.target; // if clicked element matches li, check textContent with answer
     if (element.matches("li")) {
         if (element.textContent == quizQuestions.answer) {
-            score++
+            score++ // if correct add 1 to score
         } else {
-            score--
+            score-- // if incorrect subtract score
         };
 
     };
+
+    if (questionIndex >= quizQuestions.length) {
+        gameOver(); // no more questions? gameOver 
+    } else {
+        currentQuestion++;  // add 1 to currentQuestion showing not defined.
+        makeStuffHappen(currentQuestion); // send currentquestion index number to makeStuffHappen
+    }
     
     
 };
